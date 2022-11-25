144541dfd35151cba1d7a92d3d0f5909b7b639e2
shuvo-h <shuvoh38@gmail.com>
2022-11-25 13:58:46 +0600

    added six distance social relationship problem solved

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import SocialDegreeRelation from './components/SocialDegreeRelation';
+import DegreeFIn from './components/DegreeFIn';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <SocialDegreeRelation></SocialDegreeRelation> */}
+      <DegreeFIn></DegreeFIn>
+    </div>
+  );
+}
+
+export default App;
